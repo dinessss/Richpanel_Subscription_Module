0c6fa147c369f9b629ea96f9741cee30ca2994ad
The Repositories of Dinesh <82640300+dinessss@users.noreply.github.com>
2022-07-27 01:14:33 +0530

    first commit

---
+++ index1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<div class="dropdown"> 
+		<button class="dropdown-button">Subsriptions Modes</button> 
+		<div class="dropdown-list"> 
+			<a href="index.php">Monthly</a> 
+			<a href="index1.php">Yearly</a> 
+		</div> 
+	</div> 
+
+
+<h2 style="text-align:center">Choose the Right Yearly Plan for You!!</h2>
+<br>
+
+
+<div class="headings">
+  <ul class="price">
+    <li class="header">Specifications</li>
+    <li class="grey">Price</li>
+    <li>Video Quality</li>
+    <li>Specifications</li>
+    <li>Devices you can use to watch</li>
+  </ul>
+</div>
+
+
+
+
+<div class="columns">
+  
+  <ul class="price">
+    <li class="header">Premium</li>
+    <li class="grey">7000.00 INR / Year</li>
+    <li>Best</li>
+    <li>4K+HDR</li>
+    <li>Phone<br>Tablet<br>Computer<br>TV</li>
+    <li class="grey"><a href="checkout1.php" class="button">Subscribe</a></li>
+  </ul>
+</div>
+
+<div class="columns">
+  <ul class="price">
+    <li class="header">Standard</li>
+    <li class="grey">5000.00 INR / Year</li>
+    <li>Better</li>
+    <li>1080p</li>
+    <li>Phone<br>Tablet<br>Computer<br>TV</li>
+    <li class="grey"><a href="#" class="button">Subscribe</a></li>
+  </ul>
+</div>
+
+<div class="columns">
+  <ul class="price">
+    <li class="header">Basic</li>
+    <li class="grey">2000.00 INR / Year</li>
+    <li>Good</li>
+    <li>480p</li>
+    <li>Phone<br>Tablet<br>Computer<br>TV</li>
+    <li class="grey"><a href="#" class="button">Subscribe</a></li>
+  </ul>
+</div>
+
+<div class="columns">
+  <ul class="price">
+    <li class="header">Mobile</li>
+    <li class="grey">1000.00 INR / Year</li>
+    <li>Good</li>
+    <li>480p</li>
+    <li>Phone<br>Tablet</li>
+    <li class="grey"><a href="#" class="button">Subscribe</a></li>
+  </ul>
+</div>
+
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,31 @@
+
+<html>
+  <head><title>Order status</title>
+  <link rel = "stylesheet" href = "style.css">   
+
+</head>
+  
+  <body style="background-color: #0121ad">
+  <div class="card">
+			<div id='txt'>
+   
+      <h1> &nbsp &nbsp Current Plan Details - active <button style="float:right; padding:10px; margin-right:10px;"><a href="cancel.html">CANCEL</a></button></h1>
+      <h2>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspBasic</h2>
+      <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPhone+Tablet+Computer+TV</p>
+      
+      
+      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="index.php"><button>Change Plan</button></a>
+<br>
+<br><br>
+
+&nbsp&nbsp&nbsp&nbsp  <b>Your Subscription has started on july 25-07-2022 till 25-08-2022</b>
+
+</div>
+    </div>
+
+<br>
+
+
+
+  </body>
+</html>
